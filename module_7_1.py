8aea6e7254565aa7d68860aba83e313e298fd974
Leshka60 <leshka7@live.com>
2024-07-25 21:25:35 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -11,9 +11,10 @@ class Product:
 class Shop:
     def __init__(self):
         self.__file_name = 'products.txt'
+        self.file = open(self.__file_name, 'a')
+        self.file.close()
 
     def get_products(self):
-        self.__file_name = 'products.txt'
         file = open(self.__file_name, 'r')
         goods = file.read()
         file.close()
@@ -33,7 +34,7 @@ p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2) # __str__
+print(p2)  # __str__
 
 s1.add(p1, p2, p3)
 
