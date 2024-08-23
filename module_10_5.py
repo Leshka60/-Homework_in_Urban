f46ea800bdfe1748c295ec05ca30e8f04c68e69a
Leshka60 <leshka7@live.com>
2024-08-23 16:44:36 +0300

    Add files via upload

---
+++ module_10_5.py
@@ -0,0 +1,32 @@
+from multiprocessing import Pool
+from datetime import datetime
+
+
+def read_info(name):
+    all_data = []
+    with open(name, 'r') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            all_data.append(line)
+
+
+files = [f'file {number}.txt' for number in range(1, 5)]
+
+start_time = datetime.now()
+for file_name in files:
+    read_info(file_name)
+end_time = datetime.now()
+print(f'Линейный вызов: {end_time - start_time}')
+
+# Линейный вызов: 0:00:04.559782
+
+if __name__ == '__main__':
+    with Pool(4) as p:
+        start_time = datetime.now()
+        p.map(read_info, files)
+        end_time = datetime.now()
+    print(f'Многопроцессорный вызов: {end_time - start_time}')
+
+# Многопроцессорный вызов: 0:00:01.636122
