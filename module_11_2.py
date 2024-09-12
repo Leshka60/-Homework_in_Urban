2494667fdd6eac2693c9954476a8b29c1bf39c5a
Leshka60 <leshka7@live.com>
2024-09-12 19:50:00 +0300

    Update module_11_2.py

--- module_11_2.py
+++ module_11_2.py
@@ -1,1 +1,3 @@
-
+https://github.com/Leshka60/-Homework_in_Urban/blob/main/Снимок%20экрана%202024-09-12%20185601.png
+https://github.com/Leshka60/-Homework_in_Urban/blob/main/Снимок%20экрана%202024-09-12%20185956.png
+https://github.com/Leshka60/-Homework_in_Urban/blob/main/Снимок%20экрана%202024-09-12%20190332.png
